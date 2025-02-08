19687aba47f0bb3c5e7324e808a95ced33ff7d47
reykyt <145888308+reykyt@users.noreply.github.com>
2025-02-08 15:18:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,49 +2,63 @@
 const myBtn = document.querySelector("#myBtn");
 const nameInput = document.querySelector("#name");
 const list = document.querySelector("#list");
-const divv = document.querySelector(".divv");
-const btnNo = document.querySelector(".btnNo");
-const btnDell = document.querySelector(".btnDell");
+
+
 // Обработчик события кнопки
 myBtn.addEventListener('click', () => {
-    // Создание элемента li (в памяти, на странице его пока нет)
-    const newItem = document.createElement('li');
-    // Добавить элементу newItem класс item
-    newItem.classList.add('item');
-    // Добавить элементу текстовое содержимое из поля ввода
-    newItem.textContent = nameInput.value
+    if (nameInput.value != "") {
 
-    // Добавить элемент в список
-    list.appendChild(newItem);
+        // Создание элемента li (в памяти, на странице его пока нет)
+        const newItem = document.createElement('li');
+        // Добавить элементу newItem класс item
+        newItem.classList.add('item');
+        // Добавить элементу текстовое содержимое из поля ввода
+        newItem.textContent = nameInput.value
 
-    // Очистить поле ввода
-    nameInput.value = "";
-    //создание кнопки для удаления элемента
-    let deleteButton = document.createElement("button");
-    //добавление текста к кнопке
-    deleteButton.textContent = "удалить";
+        // Добавить элемент в список
+        list.appendChild(newItem);
 
-    deleteButton.classList.add("del")
-    //добавть в эл списка
-    newItem.appendChild(deleteButton);
+        // Очистить поле ввода
+        nameInput.value = "";
+        //создание кнопки для удаления элемента
+        let deleteButton = document.createElement("button");
+        //добавление текста к кнопке
+        deleteButton.textContent = "удалить";
 
-    deleteButton.addEventListener("click", () => {
-        // list.removeChild(newItem);
-        divv.classList.add("divvv")
+        deleteButton.classList.add("del")
+        //добавть в эл списка
+        newItem.appendChild(deleteButton);
 
-    })
-    btnNo.addEventListener("click", () => {
-        // list.removeChild(newItem);
-        divv.classList.remove("divvv")
+        const divv = document.querySelector(".divv");
+        const btnNo = document.querySelector(".btnNo");
+        const btnDell = document.querySelector(".btnDell");
 
-    })
-    btnDell.addEventListener("click", (event) => {
-        // list.removeChild(newItem);
-        divv.classList.remove("divvv")
-        newItem.classList.add("new")
-        deleteButton.classList.add("btndelllle")
+        deleteButton.addEventListener("click", () => {
+            // list.removeChild(newItem);
+            divv.classList.add("divvv")
+            btnDell.addEventListener("click", deleteee)
+            function deleteee() {
+                // list.removeChild(newItem);
+                divv.classList.remove("divvv")
+                newItem.classList.add("new")
+                deleteButton.classList.add("btndelllle")
 
-    })
+            }
+
+            btnNo.addEventListener("click", () => {
+                // list.removeChild(newItem);
+                divv.classList.remove("divvv")
+                btnDell.removeEventListener("click", deleteee)
+
+            })
+        })
 
 
+
+    }
+
+    else {
+
+        alert("заполните поле!")
+    }
 })
